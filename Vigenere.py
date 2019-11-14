9f98c14133f56e107bb82060727f57c20e701224
minhlong94 <55311435+minhlong94@users.noreply.github.com>
2019-11-14 23:36:38 +0700

    Add files via upload

---
+++ Vigenere.py
@@ -0,0 +1,62 @@
+"""A simple Vigenere encryption that uses ASCII to encrypt. As usual, Vigenere encrypts only alphabet characters
+so it does not encrypt non-alphabet characters: every non-alphabet character returns itself."""
+
+
+def encryption(text_char, key_char):
+    rowplace = ord(text_char)
+    if rowplace in range(97, 123):
+        row = ord(text_char) - 96
+        col = ord(key_char) - 96
+        return chr((row + col) % 26 + 95)
+    elif rowplace in range(65, 91):
+        row = (ord(text_char) + 32) - 96
+        col = ord(key_char) - 96
+        return ((chr((row + col) % 26 + 95)).capitalize())
+    else:
+        return chr(rowplace)
+
+
+def vigenere(text, key):
+    key_letter = [c for c in key]
+    text_letter = []
+    space = []
+    for i in range(len(text)):
+        if text[i] == " ":
+            space.append(i)
+        else:
+            text_letter.append(text[i])
+
+    if len(key) < len(text):
+        for i in range(len(text) - len(key)):
+            key_letter.append(key_letter[i])
+    elif len(key) > len(text):
+        for i in range(len(key) - len(text)):
+            key_letter.pop(len(key_letter) - 1)
+
+    for i in range(len(text_letter)):
+        text_letter[i] = encryption(text_letter[i], key_letter[i])
+    for c in space:
+        text_letter.insert(c, " ")
+    encrypted = ''.join(text_letter)
+    return encrypted
+
+
+print("Welcome to Vigenere encryption.")
+i = "1"
+while i == "1":
+    while True:
+        try:
+            key = input("Input your desired key: \nNote: it must not contains a blank.\n")
+            for c in key:
+                if c == " ":
+                    raise Exception
+        except Exception:
+            print("Key contains blank character. Please input again.\n")
+        else:
+            break
+
+    text = input("Input text you want to encrypt: \n")
+    print("Your encrypted message is: {}\n".format(vigenere(text, key)))
+    i = (input("Input ""1"" to continue, anything else to exit: \n"))
+    if i != "1":
+        exit()
